98ec13702426e4694683a4dbfacc5abd397c5424
Jakub Charkiewicz <charkiewicz.jakubb@gmail.com>
2024-09-03 13:25:21 +0200

    .py added

---
+++ pass_gen.py
@@ -0,0 +1,28 @@
+import random
+from random import shuffle
+
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters= int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+my_pass = []
+
+for i in range(nr_letters):
+    x=random.choice(letters)
+    my_pass.append(x)
+for ii in range(nr_symbols):
+    y=random.choice(symbols)
+    my_pass.append(y)
+for iii in range(nr_numbers):
+    z=random.choice(numbers)
+    my_pass.append(z)
+shuffle(my_pass)
+my_password = ''.join(my_pass)
+print(my_password)
+
+
